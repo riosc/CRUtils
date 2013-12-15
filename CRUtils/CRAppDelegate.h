d8f10c7564edd5b871b6338c0e024a3ae2e4f477
Carlos Rios <rioscarlosd@gmail.com>
2013-12-15 12:38:06 -0300

    Empty Project

---
+++ CRUtils/CRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CRAppDelegate.h
+//  CRUtils
+//
+//  Created by Carlos Rios on 12/15/13.
+//  Copyright (c) 2013 Carlos Rios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CRUtils/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CRUtils
+//
+//  Created by Carlos Rios on 12/15/13.
+//  Copyright (c) 2013 Carlos Rios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CRAppDelegate class]));
+    }
+}
